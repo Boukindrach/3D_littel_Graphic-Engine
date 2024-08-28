70e758ef66486fbd04ee258f8d0cd80be8725629
Boukind Rachid <boukind.r1@gmail.com>
2024-08-28 01:43:41 +0100

    a

---
+++ 3d_little_engine/3drenderer/src/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdint.h>
+#define WIDTH 800
+#define HEIGHT 600
+
+bool is_running = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+uint32_t *color_buffer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
+
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL");
+		return false;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window)
+	{
+		fprintf(stderr, "Error creating SDL window");
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer)
+	{
+		fprintf(stderr, "Error creating SDL renderer");
+		return false;
+	}
+	return true;
+}
+
+void setup(void) {
+	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * WIDTH * HEIGHT);
+	color_buffer_texture = SDL_CreateTexture(
+			renderer,
+			SDL_PIXELFORMAT_ARGB8888,
+			SDL_TEXTUREACCESS_STREAMING,
+			WIDTH,
+			HEIGHT);
+}
+
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;
+	}
+}
+
+void update(void) {
+
+}
+
+void render_color_buffer(void) {
+	SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		(int)(WIDTH * sizeof(uint32_t))
+	);
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+	for (int y = 0; y < HEIGHT / 2; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			color_buffer [(WIDTH * y) + x] = color;
+		}
+	}
+}
+
+void render(void) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	render_color_buffer();
+	clear_color_buffer(0xFFFFFF00);
+	SDL_RenderPresent(renderer);
+}
+
+
+int main(void)
+{
+	is_running = initialize_window();
+	
+	setup();
+
+	while (is_running) {
+		process_input();
+		update();
+		render();
+	}
+
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return (0);
+}
