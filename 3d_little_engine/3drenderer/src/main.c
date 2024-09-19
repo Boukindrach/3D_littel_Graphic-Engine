51ddb1cdbd05762b92602cd36fffe7de07b150b5
Boukind Rachid <boukind.r1@gmail.com>
2024-09-19 01:27:19 +0100

    add for comments

--- 3d_little_engine/3drenderer/src/main.c
+++ 3d_little_engine/3drenderer/src/main.c
@@ -97,7 +97,8 @@ void update(void) {
 	mesh.rotation.z += 0.00;
 	mesh.scale.x += 0.002;
 	mesh.scale.y += 0.001;
-
+	
+	// Create a scale transformation matrix
 	matrix4_t scale_matrix = mat4_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
 	
 	// Loop through all mesh faces
@@ -105,12 +106,15 @@ void update(void) {
 	for (int i = 0; i < number_faces; i++) {
 		face_t mesh_face = mesh.faces[i];
 		vector3d_t face_vertices[3];
+
+		// Get the vertices for each face
 		face_vertices[0] = mesh.vertices[mesh_face.a - 1];
 		face_vertices[1] = mesh.vertices[mesh_face.b - 1];
 		face_vertices[2] = mesh.vertices[mesh_face.c - 1];
-		triangle_t projected_triangle;
 
+		triangle_t projected_triangle;
 		vector3d_t transformed_vertices[3];
+
 		// Transform vertices
 		for (int j = 0; j < 3; j++) {
 			vector4d_t transformed_vertex = vector4d_form_vec3(face_vertices[j]);
@@ -118,62 +122,74 @@ void update(void) {
 			transformed_vertex.z += 5;
 			transformed_vertices[j] = vector3d_form_vec4(transformed_vertex);
 		}
-		//
+
+		// Perform backface culling
 		if (cull_method == CULL_BACKFACE) {
 			vector3d_t vector_a = transformed_vertices[0];
 			vector3d_t vector_b = transformed_vertices[1];
 			vector3d_t vector_c = transformed_vertices[2];
 
+			// Calculate the edge vectors of the triangle
 			vector3d_t vector_ab = vector3d_sub(vector_b, vector_a);
 			vector3d_t vector_ac = vector3d_sub(vector_c, vector_a);
 
+			// Compute the normal using the corss product
 			vector3d_t normal = vector3d_cross(vector_ab, vector_ac);
-		
+			
+			// Normalize the vectors
 			vector3d_normalize(&vector_ab);
 			vector3d_normalize(&vector_ac);
 			vector3d_normalize(&normal);
 
 			vector3d_t origin = {0, 0, 0};
 
+			// Calculate the ray from the camera to the triangle
 			vector3d_t camera_ray = vector3d_sub(origin, vector_a);
-
+			
 			float aline_camera = vector3d_dot(normal, camera_ray);
-
 			if (aline_camera < 0) {
 				continue;
 			}
 		}
-		//
+
+		// Project each transformed vertex into 2D space and store in the triangle
 		for (int j = 0; j < 3; j++) {
 			vector2d_t projected_point = project(transformed_vertices[j]);
-			projected_point.x += (WIDTH / 2);
-                        projected_point.y += (HEIGHT / 1.75);
+			projected_point.x += (WIDTH / 2);  // Adjust x-position for the screen center
+                        projected_point.y += (HEIGHT / 1.75); // Adjust y-position for the screen ce
+nter
 			projected_triangle.points[j] = projected_point;
 		};
+		// Push the projected triangle into the render queue
 		array_push(triangles_to_render, projected_triangle);
 	};
 
 }
 
+// Render the traingles to the screen
 void render(void) {
 	int number_triangles = array_length(triangles_to_render);
+
+	// Loop through each triangle and render it
 	for (int i = 0; i < number_triangles; i++) {
 		triangle_t triangle = triangles_to_render[i];
+		// If the render method includes filling the triangle, draw the filled triangle
 		if (render_method == RENDER_FILL_TRIANGLE || render_method == RENDER_FILL_TRIANGLE_WIRE) {
 			draw_filled_triangle(
 				triangle.points[0].x, triangle.points[0].y,
                 		triangle.points[1].x, triangle.points[1].y,
                 		triangle.points[2].x, triangle.points[2].y,
-				0xffffFFFF
+				0xffffFFFF // Color value for the triangle fill
 					);
 		}
-
+		
+		// If the render method includes wireframe the rendering, draw the triangle wireframe
 		if (render_method == RENDER_WIRE || render_method == RENDER_FILL_TRIANGLE_WIRE) {
 			draw_triangle(
 				triangle.points[0].x, triangle.points[0].y,
 				triangle.points[1].x, triangle.points[1].y,
 				triangle.points[2].x, triangle.points[2].y,
-				0x99000009
+				0x99000009 // Color value for the wireframe
 				);
 		}
 	}
