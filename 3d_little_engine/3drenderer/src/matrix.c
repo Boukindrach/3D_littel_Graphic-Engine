23a71d20b7c74a9cd871c9f9ede07a7356732128
Boukind Rachid <boukind.r1@gmail.com>
2024-09-19 01:56:32 +0100

    a

--- 3d_little_engine/3drenderer/src/matrix.c
+++ 3d_little_engine/3drenderer/src/matrix.c
@@ -1,5 +1,6 @@
 #include "matrix.h"
 
+// Function to create and return an identity matrix (4x4)
 matrix4_t mat4_identity(void) {
 	matrix4_t m = {{
 		{1, 0, 0, 0},
@@ -10,6 +11,7 @@ matrix4_t mat4_identity(void) {
 	return m;
 }
 
+// Function to create a scaling matrix based on the provided scale factors (sx, sy, sz)
 matrix4_t mat4_scale(float sx, float sy, float sz) {
 	matrix4_t m = mat4_identity();
         m.m[0][0] = sx;
@@ -18,6 +20,7 @@ matrix4_t mat4_scale(float sx, float sy, float sz) {
 	return m;
 }
 
+// Function to multiply a 4x4 matrix (m) by a 4D vector (v)
 vector4d_t matrix4_mul_vec4(matrix4_t m, vector4d_t v) {
 	vector4d_t result;
 	result.x = m.m[0][0] * v.x + m.m[0][1] * v.y + m.m[0][2] * v.z + m.m[0][3] * v.w;
--- 3d_little_engine/3drenderer/src/matrix.h
+++ 3d_little_engine/3drenderer/src/matrix.h
@@ -3,13 +3,16 @@
 
 #include "vector.h"
 
+// Define a 4x4 matrix structure
 typedef struct {
 	float m[4][4];
 } matrix4_t;
 
+////////////////////////////////////////////////////
+// Function declarations for matrix operations
+///////////////////////////////////////////////////
 matrix4_t mat4_identity(void);
 matrix4_t mat4_scale(float sx, float sy, float sz);
 vector4d_t matrix4_mul_vec4(matrix4_t m, vector4d_t v);
-
 
 #endif
