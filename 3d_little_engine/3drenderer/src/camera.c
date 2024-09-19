4d2e9c0e86889a287eb266f51b46e626b0fb0a94
Boukind Rachid <boukind.r1@gmail.com>
2024-09-19 01:50:16 +0100

    cleaned meshfiles

--- 3d_little_engine/3drenderer/src/camera.c
+++ 3d_little_engine/3drenderer/src/camera.c
@@ -1,7 +1,11 @@
 #include "camera.h"
 
+// Define the global camera object with initial position and direction
 camera_t camera = {
+	// Camera's initial position in the world (x, y, z)
 	.position = {0, 0, 0},
+
+	// Camera's initial direction
 	.direction = {0, 0, 1}
 
 };
--- 3d_little_engine/3drenderer/src/camera.h
+++ 3d_little_engine/3drenderer/src/camera.h
@@ -3,11 +3,13 @@
 
 #include "vector.h"
 
+// Define the camera structure
 typedef struct {
 	vector3d_t position;
 	vector3d_t direction;
 } camera_t;
 
+// Declare the camera object as an extrenal variable
 extern camera_t camera;
 
 #endif
